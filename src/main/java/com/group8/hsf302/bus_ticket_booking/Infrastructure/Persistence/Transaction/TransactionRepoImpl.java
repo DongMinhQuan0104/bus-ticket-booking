776b7e5f95eed562e123911c11d8aaf863b1c7bc
Trần Nguyễn Bảo Việt <tranjohn205@gmail.com>
2026-06-26 14:01:00 +0700

    fix: add missing @Repository annotation and trigger IDE rebuild

--- src/main/java/com/group8/hsf302/bus_ticket_booking/Infrastructure/Persistence/Transaction/TransactionRepoImpl.java
+++ src/main/java/com/group8/hsf302/bus_ticket_booking/Infrastructure/Persistence/Transaction/TransactionRepoImpl.java
@@ -1,6 +1,9 @@
 package com.group8.hsf302.bus_ticket_booking.Infrastructure.Persistence.Transaction;
 
 import com.group8.hsf302.bus_ticket_booking.Domain.Repository.TransactionRepo;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class TransactionRepoImpl implements TransactionRepo {
 }
+
