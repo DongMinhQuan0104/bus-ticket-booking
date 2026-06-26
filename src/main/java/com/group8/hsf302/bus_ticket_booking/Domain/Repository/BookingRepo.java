a403ec0113e73335b02cee55dc1a7dfda57458e2
Trần Nguyễn Bảo Việt <tranjohn205@gmail.com>
2026-06-26 13:51:20 +0700

    feat: implement repository methods for payment and booking

--- src/main/java/com/group8/hsf302/bus_ticket_booking/Domain/Repository/BookingRepo.java
+++ src/main/java/com/group8/hsf302/bus_ticket_booking/Domain/Repository/BookingRepo.java
@@ -1,5 +1,9 @@
 package com.group8.hsf302.bus_ticket_booking.Domain.Repository;
 
+import com.group8.hsf302.bus_ticket_booking.Domain.Model.Booking;
+import java.util.Optional;
+import java.util.UUID;
 
-public interface BookingRepo{
+public interface BookingRepo {
+    Optional<Booking> findById(UUID id);
 }
--- src/main/java/com/group8/hsf302/bus_ticket_booking/Infrastructure/Persistence/Payment/PaymentRepoImpl.java
+++ src/main/java/com/group8/hsf302/bus_ticket_booking/Infrastructure/Persistence/Payment/PaymentRepoImpl.java
@@ -1,6 +1,18 @@
 package com.group8.hsf302.bus_ticket_booking.Infrastructure.Persistence.Payment;
 
+import com.group8.hsf302.bus_ticket_booking.Domain.Model.Payment;
 import com.group8.hsf302.bus_ticket_booking.Domain.Repository.PaymentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PaymentRepoImpl implements PaymentRepo {
+
+    @Autowired
+    private PaymentJpaRepo paymentJpaRepo;
+
+    @Override
+    public Payment save(Payment payment) {
+        return paymentJpaRepo.save(payment);
+    }
 }
